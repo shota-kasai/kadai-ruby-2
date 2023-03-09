339350dfc371a8359b6ce151b5e0eb0cf22622d7
shota.kasai <shota_kasai@r.recruit.co.jp>
2023-03-09 15:41:52 +0000

    first commit

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,17 @@
+num_max = 100
+
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    return 'FizzBuzz'
+  elsif num % 3 == 0
+    return 'Fizz'
+  elsif num % 5 == 0
+    return 'Buzz'
+  else
+    return num
+  end
+end
+
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
